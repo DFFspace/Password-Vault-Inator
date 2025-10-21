5827a6ff07b09489f054063db0c5d28aebc50718
Pasquale Dahlmanns <p.dahlmanns@dailyfreshfood.nl>
2025-10-21 15:00:03 +0200

    Refactor UI to use NavigationView and modernize app
    Replaced legacy Grid-based layout with a dynamic NavigationView for improved navigation and user experience. Updated App.xaml to include a new resource dictionary for DevWinUI controls. Enhanced MainWindow.xaml.cs with navigation logic, back button handling, and window centering.
    
    Updated the project to target .NET 10.0 and added DevWinUI package references. Removed unused assets and added new ones, including a custom app icon. Improved project structure and modernized UI components.

--- Password.Vault.Inator/MainWindow.xaml.cs
+++ Password.Vault.Inator/MainWindow.xaml.cs
@@ -1,43 +1,87 @@
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
-using Password.Vault.Inator.Classes;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Password.Vault.Inator.Pages;
 using System;
+using System.Linq;
+using Windows.UI.ApplicationSettings;
 using WinRT.Interop;
 using WinUIEx;
 
-namespace Password.Vault.Inator
+namespace Password.Vault.Inator;
+
+public sealed partial class MainWindow : WindowEx
 {
-    public sealed partial class MainWindow : WindowEx
+    private readonly AppWindow appWindow;
+
+    public MainWindow()
     {
-        private readonly AppWindow appWindow;
-        public static MainWindow Instance { get; private set; }
+        InitializeComponent();
+        appWindow = GetAppWindowForCurrentWindow();
+        appWindow.Resize(new Windows.Graphics.SizeInt32(1200, 600));
+        this.CenterOnScreen();
+        ExtendsContentIntoTitleBar = true;
+        ApplicationTitleBar.Subtitle = App.AppVersion;
 
-        public MainWindow()
+        NavigationViewControl.SelectedItem = NavigationViewControl.MenuItems.OfType<NavigationViewItem>().First();
+        ContentFrame.Navigate(
+                   typeof(HomePage),
+                   null,
+                   new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo()
+                   );
+    }
+
+    private void NavigationViewControl_ItemInvoked(NavigationView sender,
+          NavigationViewItemInvokedEventArgs args)
+    {
+        if (args.IsSettingsInvoked == true)
         {
-            Instance = this;
-            InitializeComponent();
-            appWindow = GetAppWindowForCurrentWindow();
-            appWindow.Resize(new Windows.Graphics.SizeInt32(1200, 600));
-            this.CenterOnScreen();
-            ExtendsContentIntoTitleBar = true;
-            ApplicationTitleBar.Subtitle = App.AppVersion;
+            ContentFrame.Navigate(typeof(SettingsPage), null, args.RecommendedNavigationTransitionInfo);
+        }
+        else if (args.InvokedItemContainer != null && (args.InvokedItemContainer.Tag != null))
+        {
+            Type newPage = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+            ContentFrame.Navigate(
+                   newPage,
+                   null,
+                   args.RecommendedNavigationTransitionInfo
+                   );
+        }
+    }
+
+    private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+    {
+        NavigationViewControl.IsBackEnabled = ContentFrame.CanGoBack;
+
+        if (ContentFrame.SourcePageType == typeof(SettingsPage))
+        {
+            NavigationViewControl.SelectedItem = NavigationViewControl.FooterMenuItems
+                .OfType<NavigationViewItem>()
+                .First(n => n.Tag.Equals(ContentFrame.SourcePageType.FullName.ToString()));
+        }
+        else if (ContentFrame.SourcePageType != null)
+        {
+            NavigationViewControl.SelectedItem = NavigationViewControl.MenuItems
+                .OfType<NavigationViewItem>()
+                .First(n => n.Tag.Equals(ContentFrame.SourcePageType.FullName.ToString()));
         }
 
-        private AppWindow GetAppWindowForCurrentWindow()
-        {
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            return AppWindow.GetFromWindowId(myWndId);
-        }
+        NavigationViewControl.Header = ((NavigationViewItem)NavigationViewControl.SelectedItem)?.Content?.ToString();
+    }
 
-        private void BtnAddCredsToVault_Click(object sender, RoutedEventArgs e)
+    private void TitleBar_BackRequested(Microsoft.UI.Xaml.Controls.TitleBar sender, object args)
+    {
+        if (ContentFrame.CanGoBack)
         {
-            SecretManager.AddPasswordToCredVault(
-                TextBoxResourceName.Text,
-                TextBoxUsername.Text,
-                TextBoxPassword.Password
-            );
+            ContentFrame.GoBack();
         }
+    }
+
+    private AppWindow GetAppWindowForCurrentWindow()
+    {
+        IntPtr hWnd = WindowNative.GetWindowHandle(this);
+        WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        return AppWindow.GetFromWindowId(myWndId);
     }
 }
