92ee0b49ebd81be28f9a9dd5ea39a79676440502
Pasquale Dahlmanns <p.dahlmanns@dailyfreshfood.nl>
2025-10-21 14:59:38 +0200

    Refactor: Replace MainWindow with HomePage in SecretManager
    Updated the `AddPasswordToCredVault` method to use `HomePage.Instance` instead of `MainWindow.Instance` for managing UI elements. Added a `using` directive for `Password.Vault.Inator.Pages` to reference the `HomePage` class. This change shifts the functionality from `MainWindow` to `HomePage` while maintaining the existing success and error handling logic.

--- Password.Vault.Inator/Classes/SecretManager.cs
+++ Password.Vault.Inator/Classes/SecretManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using Password.Vault.Inator.Pages;
 using System;
 using Windows.Security.Credentials;
 
@@ -8,23 +9,23 @@ public sealed partial class SecretManager
 {
     public static void AddPasswordToCredVault(string resource, string username, string password)
     {
-        MainWindow.Instance.AddCredsToVaultInfoBar.IsOpen = true;
-        MainWindow.Instance.AddCredsToVaultProgress.IsActive = true;
+        HomePage.Instance.AddCredsToVaultInfoBar.IsOpen = true;
+        HomePage.Instance.AddCredsToVaultProgress.IsActive = true;
 
         try
         {
             var vault = new PasswordVault();
             vault.Add(new PasswordCredential(resource, username, password));
-            
-            MainWindow.Instance.AddCredsToVaultProgress.IsActive = false;
-            MainWindow.Instance.AddCredsToVaultInfoBar.Severity = InfoBarSeverity.Success;
-            MainWindow.Instance.AddCredsToVaultInfoBar.Message = $"{resource} has been added to the Windows Credential Manager!";
+
+            HomePage.Instance.AddCredsToVaultProgress.IsActive = false;
+            HomePage.Instance.AddCredsToVaultInfoBar.Severity = InfoBarSeverity.Success;
+            HomePage.Instance.AddCredsToVaultInfoBar.Message = $"{resource} has been added to the Windows Credential Manager!";
         }
         catch (Exception ex)
         {
-            MainWindow.Instance.AddCredsToVaultProgress.IsActive = false;
-            MainWindow.Instance.AddCredsToVaultInfoBar.Severity = InfoBarSeverity.Error;
-            MainWindow.Instance.AddCredsToVaultInfoBar.Message = $"Error while adding {resource}: {ex.Message}";
+            HomePage.Instance.AddCredsToVaultProgress.IsActive = false;
+            HomePage.Instance.AddCredsToVaultInfoBar.Severity = InfoBarSeverity.Error;
+            HomePage.Instance.AddCredsToVaultInfoBar.Message = $"Error while adding {resource}: {ex.Message}";
             return;
         }
     }
